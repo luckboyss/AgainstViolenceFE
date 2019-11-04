54c7dd229baee20b11f45176d42dbc706a07ca1a
88384490 <31804222+88384490@users.noreply.github.com>
2019-11-04 09:31:52 +0800

    添加注册页路由

--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -1,10 +1,16 @@
 <template>
-  $END$
+  <div class="register-container">
+    <el-form>
+      <div class="title-container">
+        <h3 class="title">注册</h3>
+      </div>
+    </el-form>
+  </div>
 </template>
 
 <script>
   export default {
-    name: ''
+    name: 'register'
   }
 </script>
 
